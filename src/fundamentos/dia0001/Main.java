888aad24a84abeab7fcb4c6a10bdde88aa819da4
Igor Santos <igorhnss@gmail.com>
2026-08-07 00:17:28 -0300

    Exercicio 0001 - Cadastro de Pessoa

---
+++ src/fundamentos/dia0001/Main.java
@@ -0,0 +1,36 @@
+package fundamentos.dia0001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a cidade que você mora: ");
+        String cidade = scanner.next();
+
+        System.out.println("Digite sua profissão: ");
+        String profissao = scanner.next();
+
+        System.out.println("===== DADOS CADASTRADOS ====");
+
+        System.out.println("");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Cidade: " + cidade);
+        System.out.println("Profissão: " + profissao);
+
+        System.out.println("");
+
+        System.out.println("=============================");
+
+        scanner.close();
+    }
+}
