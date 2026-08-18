f81c78436cf41caff85af0b35916a6b2aa9a527b
Igor Santos <igorhnss@gmail.com>
2026-08-18 16:59:41 -0300

    Exercicio 004 - Cadastro de Funcionario

--- src/fundamentos/dia004/Main.java
+++ src/fundamentos/dia004/Main.java
@@ -1,4 +1,43 @@
 package fundamentos.dia004;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do funcionário: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o cargo: ");
+        String cargo = scanner.next();
+
+        System.out.println("Digite o salário: ");
+        double salario = scanner.nextDouble();
+
+        System.out.println("Digite os anos de empresa: ");
+        int anosEmpresa = scanner.nextInt();
+
+        double novoSalario = salario;
+
+        String mensagem = nome + " trabalha como " + cargo + " e possui " + anosEmpresa + " anos de empresa.";
+
+        System.out.println("==== FUNCIONÁRIO ====");
+
+        System.out.println("");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salário: " + salario);
+        System.out.println("Tempo de empresa: " + anosEmpresa + " anos");
+        System.out.println(mensagem);
+
+        System.out.println("");
+
+        System.out.println("Novo salário: " + novoSalario);
+
+
+
+    }
 }
