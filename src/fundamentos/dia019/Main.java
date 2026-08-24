79ea2ecefccccba9e86ed478e22d16e084dff746
Igor Santos <igorhnss@gmail.com>
2026-08-24 00:21:22 -0300

    Exercicio 019 - Primeiro for

---
+++ src/fundamentos/dia019/Main.java
@@ -0,0 +1,18 @@
+package fundamentos.dia019;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("==== CONTAGEM ====");
+
+        System.out.println("");
+
+        for (int n = 1; n <= 10; n++) {
+            System.out.println(n);
+        }
+
+        System.out.println("");
+
+        System.out.println("=================");
+    }
+}
