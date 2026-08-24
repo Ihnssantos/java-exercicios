1baa5d94e586d74ea34c98383eff226de21917b2
Igor Santos <igorhnss@gmail.com>
2026-08-24 10:26:56 -0300

    Exercicio 021 - Numeros pares com for

---
+++ src/fundamentos/dia021/Main.java
@@ -0,0 +1,20 @@
+package fundamentos.dia021;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("==== NÚMEROS PARES ====");
+
+        System.out.println("");
+
+        for (int n = 1; n <= 20; n++) {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+        }
+
+        System.out.println("");
+
+        System.out.println("=======================");
+    }
+}
