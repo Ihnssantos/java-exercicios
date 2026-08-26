5df664b1ca7f69ed9b900b944bb62b6e37f60362
Igor Santos <igorhnss@gmail.com>
2026-08-26 18:54:51 -0300

    Exercicio 030 - Contagem regressiva com while

---
+++ src/fundamentos/dia030/Main.java
@@ -0,0 +1,21 @@
+package fundamentos.dia030;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("==== CONTAGEM REGRESSIVA ====");
+
+        System.out.println("");
+
+        int i = 10;
+
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+
+        System.out.println("");
+
+        System.out.println("==============================");
+    }
+}
