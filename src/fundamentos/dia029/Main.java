8c172eba1d333f21bb9de4354b794284912307fb
Igor Santos <igorhnss@gmail.com>
2026-08-26 18:48:54 -0300

    Exercicio 029 - Introducao ao while

---
+++ src/fundamentos/dia029/Main.java
@@ -0,0 +1,21 @@
+package fundamentos.dia029;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("==== CONTAGEM ====");
+
+        System.out.println("");
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("");
+
+        System.out.println("==================");
+    }
+}
