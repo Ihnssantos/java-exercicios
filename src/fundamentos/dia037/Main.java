39bd55a00dba419d544b4eaffe10dbd797344b43
Igor Santos <igorhnss@gmail.com>
2026-08-27 00:19:39 -0300

    Exercicio 037 - Contador com do while

---
+++ src/fundamentos/dia037/Main.java
@@ -0,0 +1,21 @@
+package fundamentos.dia037;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("==== CONTAGEM ====");
+
+        System.out.println("");
+
+        int contador = 1;
+
+        do {
+            System.out.println(contador);
+            contador++;
+        } while (contador <= 10);
+
+        System.out.println("");
+
+        System.out.println("=================");
+    }
+}
