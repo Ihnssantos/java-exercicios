4204605bc01673aefe3040838d07ca5b5ecf0212
Igor Santos <igorhnss@gmail.com>
2026-08-27 21:38:50 -0300

    Exercicio 040 - Percorrendo array com for

--- src/fundamentos/dia040/Main.java
+++ src/fundamentos/dia040/Main.java
@@ -1,4 +1,20 @@
 package fundamentos.dia040;
 
 public class Main {
+    static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        System.out.println("==== ELEMENTOS DO ARRAY ====");
+
+        System.out.println("");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        };
+
+        System.out.println("");
+
+        System.out.println("===============================");
+    }
 }
