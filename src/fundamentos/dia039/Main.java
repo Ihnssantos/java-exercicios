8a916b016d4b56a495937dfdec46dd306938b576
Igor Santos <igorhnss@gmail.com>
2026-08-27 21:30:19 -0300

    Exercicio 039 - Primeiro contato com arrays

---
+++ src/fundamentos/dia039/Main.java
@@ -0,0 +1,20 @@
+package fundamentos.dia039;
+
+public class Main {
+    static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        System.out.println("==== ARRAY ====");
+
+        System.out.println("");
+
+        System.out.println("Primeiro número: " + numeros[0]);
+        System.out.println("Terceiro número: " + numeros[2]);
+        System.out.println("Quinto número: " + numeros[4]);
+
+        System.out.println("");
+
+        System.out.println("================");
+    }
+}
