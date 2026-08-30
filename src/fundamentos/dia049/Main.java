e4eceaef17bfc8795008ec378a535bfb84367706
Igor Santos <igorhnss@gmail.com>
2026-08-30 19:09:12 -0300

    Exercicio 049 - Métodos toUpperCase e toLowerCase

---
+++ src/fundamentos/dia049/Main.java
@@ -0,0 +1,20 @@
+package fundamentos.dia049;
+
+public class Main {
+    static void main(String[] args) {
+
+        String nome = "Igor";
+
+        System.out.println("==== STRING ====");
+
+        System.out.println("");
+
+        System.out.println("Nome original: " + nome);
+        System.out.println("Nome em maiúsculo: " + nome.toUpperCase());
+        System.out.println("Nome em minúsculo: " + nome.toLowerCase());
+
+        System.out.println("");
+
+        System.out.println("=================");
+    }
+}
