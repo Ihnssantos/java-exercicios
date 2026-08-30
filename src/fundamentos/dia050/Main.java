be6a464c4e7eec4b8319494c114cf4afb8aea7b9
Igor Santos <igorhnss@gmail.com>
2026-08-30 19:15:06 -0300

    Exercicio 050 - Metodo charAt

---
+++ src/fundamentos/dia050/Main.java
@@ -0,0 +1,21 @@
+package fundamentos.dia050;
+
+public class Main {
+    static void main(String[] args) {
+
+        String nome = "Igor";
+
+        System.out.println("==== CARACTERE ====");
+
+        System.out.println("");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Primeiro caractere: " + nome.charAt(0));
+        System.out.println("Segundo caractere: " + nome.charAt(1));
+        System.out.println("Ultimo caractere: " + nome.charAt(3));
+
+        System.out.println("");
+
+        System.out.println("=====================");
+    }
+}
