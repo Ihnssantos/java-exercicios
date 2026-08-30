4ad5ffb23d4c523b77743804461fc1aeb4386a28
Igor Santos <igorhnss@gmail.com>
2026-08-30 18:49:56 -0300

    String e length

---
+++ src/fundamentos/dia048/Main.java
@@ -0,0 +1,19 @@
+package fundamentos.dia048;
+
+public class Main {
+    static void main(String[] args) {
+
+        String nome = "Igor";
+
+        System.out.println("==== STRING ====");
+
+        System.out.println("");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Quantidade de caracteres: " + nome.length());
+
+        System.out.println("");
+
+        System.out.println("==================");
+    }
+}
