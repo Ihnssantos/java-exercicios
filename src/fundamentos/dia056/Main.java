677a7c8b8154f98bf344b48757d3cc6669875617
Igor Santos <igorhnss@gmail.com>
2026-08-31 20:35:17 -0300

    Exercicio 056 - Metodo com dois parametros

---
+++ src/fundamentos/dia056/Main.java
@@ -0,0 +1,13 @@
+package fundamentos.dia056;
+
+public class Main {
+    static void main(String[] args) {
+
+        apresentar("Igor", 25);
+    }
+
+    static void apresentar(String nome, int idade) {
+        System.out.println("Olá, " + nome);
+        System.out.println("Você tem " + idade);
+    }
+}
