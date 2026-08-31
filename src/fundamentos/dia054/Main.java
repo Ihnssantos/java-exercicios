b51521e8f530a70fe7def2a9c904b431edf27d6b
Igor Santos <igorhnss@gmail.com>
2026-08-31 20:29:06 -0300

    Exercicio 054 - Primeiro metodo

---
+++ src/fundamentos/dia054/Main.java
@@ -0,0 +1,11 @@
+package fundamentos.dia054;
+
+public class Main {
+    static void main(String[] args) {
+        mostrarMensagem();
+    }
+
+    static void mostrarMensagem() {
+        System.out.println("Olá! Estou aprendendo Java!");
+    }
+}
