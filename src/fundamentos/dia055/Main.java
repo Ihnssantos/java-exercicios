7dcf87fded23b9a695400d36b9c17bfc83cd11b7
Igor Santos <igorhnss@gmail.com>
2026-08-31 20:32:27 -0300

    Exercicio 055 - Metodo com parametro

---
+++ src/fundamentos/dia055/Main.java
@@ -0,0 +1,12 @@
+package fundamentos.dia055;
+
+public class Main {
+    static void main(String[] args) {
+
+        cumprimentar("Igor");
+    }
+
+    static void cumprimentar(String nome) {
+        System.out.println("Olá, " + nome);
+    }
+}
