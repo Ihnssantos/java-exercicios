6b5b816ced292b2837341a918fd58023d9bf4495
Igor Santos <igorhnss@gmail.com>
2026-09-06 17:33:53 -0300

    Exercicio 064 - Construtor

---
+++ src/poo/dia064/Main.java
@@ -0,0 +1,11 @@
+package poo.dia064;
+
+public class Main {
+    static void main(String[] args) {
+
+        Produto produto1 = new Produto("Mouse", 100.0);
+
+        System.out.println("Nome: " + produto1.nome);
+        System.out.println("Preço: " + produto1.preco);
+    }
+}
---
+++ src/poo/dia064/Produto.java
@@ -0,0 +1,12 @@
+package poo.dia064;
+
+public class Produto {
+
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
