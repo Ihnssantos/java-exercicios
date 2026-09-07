c20633edadb3f79de7c4485532defd4c90998f13
Igor Santos <igorhnss@gmail.com>
2026-09-07 16:11:35 -0300

    Encapsulamento e private

---
+++ src/poo/dia066/Main.java
@@ -0,0 +1,10 @@
+package poo.dia066;
+
+public class Main {
+    static void main(String[] args) {
+
+        Pessoa pessoa1 = new Pessoa("Igor", 25);
+
+        System.out.println("Nome: " + pessoa1.nome);
+    }
+}
