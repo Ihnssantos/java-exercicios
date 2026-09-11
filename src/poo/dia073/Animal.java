ddf7d189777a23c50ef6a4be44262273c71e3855
Igor Santos <igorhnss@gmail.com>
2026-09-10 23:36:09 -0300

    Sobrescrita de metodos

---
+++ src/poo/dia073/Animal.java
@@ -0,0 +1,17 @@
+package poo.dia073;
+
+public class Animal {
+    private String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void emitirSom() {
+        System.out.println("O animal emitiu um som!");
+    }
+}
---
+++ src/poo/dia073/Main.java
@@ -0,0 +1,10 @@
+package poo.dia073;
+
+public class Main {
+    static void main(String[] args) {
+
+        Cachorro cachorro1 = new Cachorro("Mel");
+
+        cachorro1.emitirSom();
+    }
+}
